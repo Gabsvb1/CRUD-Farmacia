000b355352de5ab0c638fe20dbc63422ffb3c844
Gabriella Vieira Bruno <gabi_bvieira@hotmail.com>
2024-01-31 11:34:57 -0300

    CRUD-Produto e testes

--- src/main/java/com/generation/crudfarmacia/model/Categoria.java
+++ src/main/java/com/generation/crudfarmacia/model/Categoria.java
@@ -1,9 +1,16 @@
 package com.generation.crudfarmacia.model;
 
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
@@ -22,9 +29,21 @@ public class Categoria {
 	@NotNull(message = "O Atributo Descrição é obrigatório")
 	@Size(max = 255, message = " A descrição deve conter no máximo 255 caracteres")
 	private String descricao;
+	
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "categoria", cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties("categoria")
+	private List<Produto> produto;
 
 	public Long getId() {
 		return id;
+	}
+
+	public List<Produto> getProduto() {
+		return produto;
+	}
+
+	public void setProduto(List<Produto> produto) {
+		this.produto = produto;
 	}
 
 	public void setId(Long id) {
---
+++ src/main/java/com/generation/crudfarmacia/model/Produto.java
@@ -0,0 +1,88 @@
+package com.generation.crudfarmacia.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name= "tb_produto")
+public class Produto {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank(message = "O atributo Nome é obrigatório!")
+	private String nome;
+	
+	private double preco;
+	
+	@NotBlank(message = "O atributo Descrição é obrigatório!")
+	@Size(max = 255, message = "O atributo Descrição deve conter no máximo 255 caracteres")
+	private String descricao;
+	
+	@NotBlank(message = "O atributo produtores é obrigatório!")
+	private String produtor; 
+	
+	@ManyToOne
+	@JsonIgnoreProperties("produto")
+	private Categoria categoria;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getProdutor() {
+		return produtor;
+	}
+
+	public void setProdutor(String produtor) {
+		this.produtor = produtor;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+	
+	
+	
+}
